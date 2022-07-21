ff7d79759362d28d8e9f76cde9fb30353aa330f7
Paulo Filho <pcf.feat@gmail.com>
2022-07-21 14:49:04 -0400

    test: resolve jest errors with react navigation by adding official mocks

--- src/screens/ShowDetailsScreen/components/EpisodeList/__tests__/EpisodeList.test.tsx
+++ src/screens/ShowDetailsScreen/components/EpisodeList/__tests__/EpisodeList.test.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import {render} from '@testing-library/react-native';
 import {EpisodeList} from '../EpisodeList';
 import {mocks} from './mocks';
+import {NavigationContainer} from '@react-navigation/native';
 
 describe('EpisodeList', () => {
   test('the component rendered', () => {
-    render(<EpisodeList show={mocks.show} />);
+    render(
+      <NavigationContainer>
+        <EpisodeList show={mocks.show} />
+      </NavigationContainer>,
+    );
 
     expect(true).toBeTruthy();
   });
