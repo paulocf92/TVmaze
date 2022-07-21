70be9cc4b502a22333d1776415acbe5c1efecda5
Paulo Filho <pcf.feat@gmail.com>
2022-07-21 15:37:23 -0400

    test: avoid interleaved error when performing sequential findBy* queries by replacing it with getBy*

--- src/screens/ShowDetailsScreen/components/EpisodeList/__tests__/EpisodeList.test.tsx
+++ src/screens/ShowDetailsScreen/components/EpisodeList/__tests__/EpisodeList.test.tsx
@@ -25,12 +25,14 @@ describe('EpisodeList', () => {
         2: [mocks.episode22, mocks.episode23],
       },
     });
-    const {findByText} = render(<EpisodeList show={mocks.show} />, {
+    const {findByText, getByText} = render(<EpisodeList show={mocks.show} />, {
       wrapper,
     });
 
     const episode1 = await findByText(mocks.episode1.name);
+    const episode2 = getByText(mocks.episode2.name);
 
     expect(episode1).toBeTruthy();
+    expect(episode2).toBeTruthy();
   });
 });
