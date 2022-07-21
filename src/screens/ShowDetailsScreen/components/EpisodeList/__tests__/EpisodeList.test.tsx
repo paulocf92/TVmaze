affc228c9e60c5d8ff3265afec038a38e31c8f14
Paulo Filho <pcf.feat@gmail.com>
2022-07-21 15:06:38 -0400

    test: resolve react query tests by applying a wrapper

--- src/screens/ShowDetailsScreen/components/EpisodeList/__tests__/EpisodeList.test.tsx
+++ src/screens/ShowDetailsScreen/components/EpisodeList/__tests__/EpisodeList.test.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import {render} from '@testing-library/react-native';
+import {QueryClient, QueryClientProvider} from 'react-query';
 import {EpisodeList} from '../EpisodeList';
 import {mocks} from './mocks';
 
+const queryClient = new QueryClient();
+const wrapper = ({children}: {children: React.ReactNode}) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
 describe('EpisodeList', () => {
   test('the component rendered', () => {
-    render(<EpisodeList show={mocks.show} />);
+    render(<EpisodeList show={mocks.show} />, {wrapper});
 
     expect(true).toBeTruthy();
   });
