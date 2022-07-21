538d1e4c87d281a4c6a17ff565df1abf5603a7e3
Paulo Filho <pcf.feat@gmail.com>
2022-07-21 14:36:17 -0400

    test: set up async storage mock via jest setup file

---
+++ src/screens/ShowDetailsScreen/components/EpisodeList/__tests__/EpisodeList.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import {EpisodeList} from '../EpisodeList';
+import {mocks} from './mocks';
+
+describe('EpisodeList', () => {
+  test('the component rendered', () => {
+    render(<EpisodeList show={mocks.show} />);
+  });
+});
