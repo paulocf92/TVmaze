d364f97f131687c988b960e2dd12c0ca5b70f579
Paulo Filho <pcf.feat@gmail.com>
2022-07-21 13:55:41 -0400

    test: test getEpisodes output grouped by season

--- src/services/show/__tests__/showService.test.ts
+++ src/services/show/__tests__/showService.test.ts
@@ -1,23 +1,30 @@
 import {api} from '../../api';
 import {showService} from '../showService';
-import {episodeList} from './mocks';
+import {episode1, episode2, episode22, episode23, episodeList} from './mocks';
 
 describe('showService', () => {
   describe('getEpisodes', () => {
-    test('when API return episode list, return a list of episodes grouped by season', async () => {
-      const spyFn = jest.spyOn(api, 'get').mockImplementation(() =>
-        Promise.resolve({
-          data: episodeList,
-        }),
-      );
+    test('when API return episode list, return all season names', async () => {
+      jest.spyOn(api, 'get').mockResolvedValue({data: episodeList});
       const groupedEpisodes = await showService.getEpisodes('250');
 
       expect(groupedEpisodes.seasonNames.includes('1')).toBeTruthy();
       expect(groupedEpisodes.seasonNames.includes('2')).toBeTruthy();
 
       expect(groupedEpisodes.seasonNames.length).toBe(2);
+    });
+    test('when API return episode list, return episodes grouped by season', async () => {
+      jest.spyOn(api, 'get').mockResolvedValue({data: episodeList});
+      const groupedEpisodes = await showService.getEpisodes('250');
 
-      expect(spyFn).toBeCalledTimes(1);
+      const season1 = groupedEpisodes.seasons[1];
+      const season2 = groupedEpisodes.seasons[2];
+
+      expect(season1.includes(episode1)).toBeTruthy();
+      expect(season1.includes(episode2)).toBeTruthy();
+
+      expect(season2.includes(episode22)).toBeTruthy();
+      expect(season2.includes(episode23)).toBeTruthy();
     });
   });
 });
