e9564f748f43b702866eb71b6dd1e641d02b05e7
Paulo Filho <pcf.feat@gmail.com>
2022-07-21 15:13:46 -0400

    test: mock react query api service request resolved value

--- src/screens/ShowDetailsScreen/components/EpisodeList/__tests__/EpisodeList.test.tsx
+++ src/screens/ShowDetailsScreen/components/EpisodeList/__tests__/EpisodeList.test.tsx
@@ -3,6 +3,7 @@ import {render} from '@testing-library/react-native';
 import {QueryClient, QueryClientProvider} from 'react-query';
 import {EpisodeList} from '../EpisodeList';
 import {mocks} from './mocks';
+import {showService} from '../../../../../services/show/showService';
 
 const queryClient = new QueryClient();
 const wrapper = ({children}: {children: React.ReactNode}) => (
@@ -11,6 +12,12 @@ const wrapper = ({children}: {children: React.ReactNode}) => (
 
 describe('EpisodeList', () => {
   test('the component rendered', () => {
+    jest.spyOn(showService, 'getEpisodes').mockResolvedValueOnce({
+      seasonNames: [],
+      seasons: {
+        1: [],
+      },
+    });
     render(<EpisodeList show={mocks.show} />, {wrapper});
 
     expect(true).toBeTruthy();
