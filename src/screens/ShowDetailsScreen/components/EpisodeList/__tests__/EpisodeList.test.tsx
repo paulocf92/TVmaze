1377ea692f61357889f7a3cabbc3beb0200fb71c
Paulo Filho <pcf.feat@gmail.com>
2022-07-21 14:57:26 -0400

    test: mock useNavigation implementation natively

--- src/screens/ShowDetailsScreen/components/EpisodeList/__tests__/EpisodeList.test.tsx
+++ src/screens/ShowDetailsScreen/components/EpisodeList/__tests__/EpisodeList.test.tsx
@@ -2,15 +2,10 @@ import React from 'react';
 import {render} from '@testing-library/react-native';
 import {EpisodeList} from '../EpisodeList';
 import {mocks} from './mocks';
-import {NavigationContainer} from '@react-navigation/native';
 
 describe('EpisodeList', () => {
   test('the component rendered', () => {
-    render(
-      <NavigationContainer>
-        <EpisodeList show={mocks.show} />
-      </NavigationContainer>,
-    );
+    render(<EpisodeList show={mocks.show} />);
 
     expect(true).toBeTruthy();
   });
