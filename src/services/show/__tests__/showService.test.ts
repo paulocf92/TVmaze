6a75f78afa4819cffec4cde10ddaa3034092d5b5
Paulo Filho <pcf.feat@gmail.com>
2022-07-21 13:44:02 -0400

    test: test getEpisodes with a mocked implementation that replaces api output

--- src/services/show/__tests__/showService.test.ts
+++ src/services/show/__tests__/showService.test.ts
@@ -1,13 +1,21 @@
 import {api} from '../../api';
 import {showService} from '../showService';
+import {episodeList} from './mocks';
 
 describe('showService', () => {
   describe('getEpisodes', () => {
     test('when API return episode list, return a list of episodes grouped by season', async () => {
-      const spyFn = jest.spyOn(api, 'get');
+      const spyFn = jest.spyOn(api, 'get').mockImplementation(() =>
+        Promise.resolve({
+          data: episodeList,
+        }),
+      );
       const groupedEpisodes = await showService.getEpisodes('250');
 
-      console.log('episode:', groupedEpisodes.seasons[1][0]);
+      expect(groupedEpisodes.seasonNames.includes('1')).toBeTruthy();
+      expect(groupedEpisodes.seasonNames.includes('2')).toBeTruthy();
+
+      expect(groupedEpisodes.seasonNames.length).toBe(2);
 
       expect(spyFn).toBeCalledTimes(1);
     });
