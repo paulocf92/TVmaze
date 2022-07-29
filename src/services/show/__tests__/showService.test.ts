f6259a8456d1507aa092600e650e61497af26df6
Paulo Filho <pcf.feat@gmail.com>
2022-07-29 08:18:28 -0400

    test: replace spyOn mockResolvedValue by msw intercepted value

--- src/services/show/__tests__/showService.test.ts
+++ src/services/show/__tests__/showService.test.ts
@@ -1,12 +1,8 @@
-import {api} from '../../api';
+import {showMocks} from 'test/mocks/showMocks';
 import {showService} from '../showService';
-import {episode1, episode2, episode22, episode23, episodeList} from './mocks';
 
 describe('showService', () => {
   describe('getEpisodes', () => {
-    beforeAll(() => {
-      jest.spyOn(api, 'get').mockResolvedValue({data: episodeList});
-    });
     test('when API return episode list, return all season names', async () => {
       const groupedEpisodes = await showService.getEpisodes('250');
 
@@ -21,11 +17,11 @@ describe('showService', () => {
       const season1 = groupedEpisodes.seasons[1];
       const season2 = groupedEpisodes.seasons[2];
 
-      expect(season1.includes(episode1)).toBeTruthy();
-      expect(season1.includes(episode2)).toBeTruthy();
+      expect(season1[0]).toEqual(showMocks.episode1);
+      expect(season1[1]).toEqual(showMocks.episode2);
 
-      expect(season2.includes(episode22)).toBeTruthy();
-      expect(season2.includes(episode23)).toBeTruthy();
+      expect(season2[0]).toEqual(showMocks.episode22);
+      expect(season2[1]).toEqual(showMocks.episode23);
     });
   });
 });
